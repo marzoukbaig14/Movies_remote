b841320b57853e8d4fa42b7c069b63b29a5ccf80
Muhammad Marzouk Baig <mbaig2@u.rochester.edu>
2023-09-08 11:22:18 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Baig, Muhammad on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
